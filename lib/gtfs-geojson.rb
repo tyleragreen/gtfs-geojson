fd8ec7bab0463a1a3eff46847b087ec84ee68612
tgreen8091 <tgreen8091@gmail.com>
2016-03-12 23:56:02 +0000

    Update tests for valid and invalid input

--- lib/gtfs-geojson.rb
+++ lib/gtfs-geojson.rb
@@ -8,11 +8,15 @@ module GTFS
   module GeoJSON
 
     def self.generate(path)
+      raise ArgumentError if !File.exist?(path)
+
+      # Confirm the GTFS feed is valid
       source = GTFS::Source.build(path,{strict:true})
       
       routes   = {}
       features = []
-      
+     
+      # Group the routes by the route id 
       source.shapes.each do |shape|
         if routes[shape.id]
           routes[shape.id] << shape
@@ -21,14 +25,21 @@ module GTFS
         end
       end
       
+      # Process the data a route at a time
       routes.each do |route,points|
         coordinates = []
+
+        # Sort the points by their point sequence ID, as it may be
+        # out of order in the GTFS feed, but must be in order to appear
+        # correct for GeoJSON
         points.sort! { |a,b| a.pt_sequence.to_i <=> b.pt_sequence.to_i }
       
+        # Add each sorted point to a list of coordinates for this route
         points.each do |point|
           coordinates << [ point.pt_lon.to_f, point.pt_lat.to_f ]
         end
-      
+
+        # Construct a feature and add it to the feature list for this route
         features << { type: 'Feature',
                       properties: { name: route
                                   },
@@ -39,6 +50,8 @@ module GTFS
       
       end
       
+      # Create the GeoJSON file by generating a JSON string wrapped 
+      # in a FeatureCollection
       return JSON.generate({type: 'FeatureCollection', features: features})
     end
   end
--- test/test.rb
+++ test/test.rb
@@ -3,8 +3,17 @@
 require_relative 'test_helper'
 
 class TestGtfs < Minitest::Test
+
+  def test_invalid_file_fails_cleanly
+    assert_raises(ArgumentError) { GTFS::GeoJSON.generate("non_existent_file") }
+  end
+
   def test_valid_gtfs
-    assert_equal String, GTFS::GeoJSON.generate("test/fixtures/valid_gtfs.zip").class
+    geojson = GTFS::GeoJSON.generate("test/fixtures/valid_gtfs.zip")
+    shapes  = File.read("test/fixtures/shapes.geojson")
+
+    assert_equal String, geojson.class
+    assert_equal shapes, geojson
   end
     
   def test_invalid_gtfs
